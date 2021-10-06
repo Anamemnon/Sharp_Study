0ccec3593cf0f56442e143ff7c0324adbe425536
Dmitry Chekir <chekirdmitry@gmail.com>
2021-10-06 21:43:24 +0300

    734A - Anton and Danik

---
+++ codeforces/A/734A - Anton and Danik/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Main
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.ReadLine();
+
+            int winner = 0;
+
+            foreach (var ch in Console.ReadLine() ?? "")
+            {
+                if (ch == 'A') winner++;
+                else winner--;
+            }
+
+            Console.WriteLine(winner > 0 ? "Anton" : winner < 0 ? "Danik" : "Friendship");
+        }
+    }
+}
