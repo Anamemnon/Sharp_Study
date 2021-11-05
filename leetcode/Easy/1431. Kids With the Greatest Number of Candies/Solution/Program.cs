67016d9a349b9d801f7fe438b7eb9efbd969390d
Dmitry Chekir <chekirdmitry@gmail.com>
2021-11-05 08:03:57 +0300

    1431. Kids With the Greatest Number of Candies

---
+++ leetcode/Easy/1431. Kids With the Greatest Number of Candies/Solution/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Solution
+{
+    public IList<bool> KidsWithCandies(int[] candies, int extraCandies)
+    {
+        var max = candies.Max();
+        IList<bool> result = candies
+            .Select(cand => cand + extraCandies >= max)
+            .ToList<bool>();
+        return result;
+    }
+}
