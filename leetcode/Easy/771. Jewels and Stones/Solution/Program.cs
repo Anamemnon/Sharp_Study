5bc6e9e81f6b279a1502365fe9e86651b0c5c7c1
Dmitry Chekir <chekirdmitry@gmail.com>
2021-11-06 10:09:05 +0300

    771. Jewels and Stones

---
+++ leetcode/Easy/771. Jewels and Stones/Solution/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Solution
+{
+    public int NumJewelsInStones(string jewels, string stones)
+    {
+        var setJewels = new HashSet<char>(jewels);
+        int result = 0;
+
+        foreach (char jewel in stones)
+        {
+            if (setJewels.Contains(jewel)) result++;
+        }
+
+        return result;
+    }
+}
