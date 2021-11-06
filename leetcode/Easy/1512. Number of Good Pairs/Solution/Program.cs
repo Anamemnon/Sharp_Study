a9fc746c39378a202b64f0cbc821a4dda6e8fbb8
Dmitry Chekir <chekirdmitry@gmail.com>
2021-11-06 09:59:57 +0300

    1512. Number of Good Pairs

---
+++ leetcode/Easy/1512. Number of Good Pairs/Solution/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Solution
+{
+    public int NumIdenticalPairs(int[] nums)
+    {
+        int[] counts = new int[101];
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            counts[nums[i]]++;
+        }
+
+        return counts.Sum(x => x * (x - 1) / 2);
+    }
+}
