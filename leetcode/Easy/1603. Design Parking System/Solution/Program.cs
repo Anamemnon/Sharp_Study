401c3770b9429c1b6643d83de0faf285527f6ccc
Dmitry Chekir <chekirdmitry@gmail.com>
2021-11-07 17:22:11 +0300

    1603. Design Parking System

---
+++ leetcode/Easy/1603. Design Parking System/Solution/Program.cs
@@ -0,0 +1,50 @@
+﻿public class ParkingSystem
+{
+    public static int Big { get; set; }
+    public static int Medium { get; set; }
+    public static int Small { get; set; }
+
+    public ParkingSystem(int big, int medium, int small)
+    {
+        Big = big;
+        Medium = medium;
+        Small = small;
+    }
+
+    public bool AddCar(int carType)
+    {
+        switch (carType)
+        {
+            case 1:
+                {
+                    if (Big > 0)
+                    {
+                        Big--;
+                        return true;
+                    }
+                    return false;
+                }
+            case 2:
+                {
+                    if (Medium > 0)
+                    {
+                        Medium--;
+                        return true;
+                    }
+                    return false;
+                }
+            case 3:
+                {
+                    if (Small > 0) { Small--; return true; }
+                    return false;
+                }
+            default: return false;
+        }
+    }
+}
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem obj = new ParkingSystem(big, medium, small);
+ * bool param_1 = obj.AddCar(carType);
+ */
