8c8b3c8efbc64be6d32ae375c60b46b9e4f5cc24
Dmitry Chekir <chekirdmitry@gmail.com>
2021-12-14 22:28:09 +0300

    Models have been added. Migration has been done.

---
+++ web/razor/RazorPagesMovie/RazorPagesMovie/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Models;
+
+namespace RazorPagesMovie.Pages.Movies
+{
+#pragma warning disable CS8618
+#pragma warning disable CS8604
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPagesMovie.Data.RazorPagesMovieContext _context;
+
+        public IndexModel(RazorPagesMovie.Data.RazorPagesMovieContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+#pragma warning disable CS8618
+#pragma warning disable CS8604
+}
