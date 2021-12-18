58b525ade5714bbc6a707f04098c4c90e9f059e5
Dmitry Chekir <chekirdmitry@gmail.com>
2021-12-18 09:59:15 +0300

    Seach has been added

--- web/razor/RazorPagesMovie/RazorPagesMovie/Pages/Movies/Index.cshtml.cs
+++ web/razor/RazorPagesMovie/RazorPagesMovie/Pages/Movies/Index.cshtml.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.Models;
 
@@ -17,9 +19,29 @@ namespace RazorPagesMovie.Pages.Movies
 
         public IList<Movie> Movie { get; set; }
 
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        public SelectList Genres { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string MovieGenre { get; set; }
+
         public async Task OnGetAsync()
         {
-            Movie = await _context.Movie.ToListAsync();
+            var movies = _context.Movie.Select(movie => movie);
+
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(SearchString));
+            }
+
+            if (!string.IsNullOrEmpty(MovieGenre))
+            {
+                movies = movies.Where(x => x.Genre == MovieGenre);
+            }
+            // Use LINQ to get list of genres.
+            IQueryable<string> genreQuery = _context.Movie.Select(movie => movie.Genre).OrderBy(genre => genre);
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+            Movie = await movies.ToListAsync();
         }
     }
 #pragma warning disable CS8618
