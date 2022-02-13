4d47acf0d694caa2dedfee150d3ec29a81613a61
Dmitry Chekir <chekirdmitry@gmail.com>
2022-02-13 11:36:21 +0300

    94. Binary Tree Inorder Traversal

---
+++ leetcode/Easy/94. Binary Tree Inorder Traversal/Solution/Solution/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Solution;
+
+
+#nullable disable
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution
+{
+    public IList<int> InorderTraversal(TreeNode root)
+    {
+        var result = new List<int>();
+        var stack = new Stack<TreeNode>();
+        while (stack.Count != 0 || root != null)
+        {
+            if (root != null)
+            {
+                stack.Push(root);
+                root = root.left;
+            }
+            else
+            {
+                root = stack.Pop();
+                result.Add(root.val);
+                root = root.right;
+            }
+        }
+
+        return result;
+    }
+}
