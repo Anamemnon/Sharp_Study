8abe0fb037fe43e03d8db0c310c171aed4fd0f68
Dmitry Chekir <chekirdmitry@gmail.com>
2022-02-13 18:04:37 +0300

    108. Convert Sorted Array to Binary Search Tree

---
+++ leetcode/Easy/108. Convert Sorted Array to Binary Search Tree/Solution/Program.cs
@@ -0,0 +1,29 @@
+﻿#nullable disable
+public class TreeNode {
+     public int val;
+     public TreeNode left;
+     public TreeNode right;
+     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+     }
+ }
+
+public class Solution {
+    public TreeNode SortedArrayToBST(int[] nums) {
+        TreeNode answer = null;
+        
+        if (nums == null || nums.Count() == 0){
+            return answer;
+        }
+        int length = nums.Count();
+        
+        answer = new TreeNode(nums[length/2]);
+        
+        answer.left = SortedArrayToBST(nums.Take(length/2).ToArray());
+        answer.right = SortedArrayToBST(nums.Skip(length/2 + 1).Take(length - (length/2 + 1)).ToArray());
+        
+        return answer;
+    }
+}
