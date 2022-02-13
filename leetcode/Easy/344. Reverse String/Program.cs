496ad2438e926918b15858b74e2f8b60f8dd713f
Dmitry Chekir <chekirdmitry@gmail.com>
2022-02-13 10:13:46 +0300

    344. Reverse String

---
+++ leetcode/Easy/344. Reverse String/Program.cs
@@ -0,0 +1,13 @@
+﻿var l = new[] {'h', 'e', 'l', 'l', 'o'};
+ReverseString(l);
+foreach (var item in l)
+{
+    Console.Write(item);
+}
+
+void ReverseString(char[] s) {
+    for (var i = 0; i < s.Length / 2; i++)
+    {
+        (s[i], s[^(i+1)]) = (s[^(i+1)], s[i]);
+    }
+}
