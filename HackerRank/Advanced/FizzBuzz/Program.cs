846abce7f3a11f17a6c60d62818552fd80a25f28
Dmitry Chekir <chekirdmitry@gmail.com>
2022-04-29 22:22:14 +0300

    [+] FizzBuzz

---
+++ HackerRank/Advanced/FizzBuzz/Program.cs
@@ -0,0 +1,15 @@
+﻿fizzBuzz(100);
+
+Enumerable.Range(1, 100)
+    .Select(fizzBuzz).ToList()
+    .ForEach(Console.WriteLine);
+
+string fizzBuzz(int i) =>
+    (i % 3 == 0, i % 5 == 0) switch
+    {
+        (true, true) => "FizzBuzz",
+        (true, _) => "Fizz",
+        (_, true) => "Buzz",
+        _ => i.ToString()
+    };
+
