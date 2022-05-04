fbbc2fb8f95640d3137a2af72054834b5a364314
Dmitry Chekir <chekirdmitry@gmail.com>
2022-05-05 00:10:07 +0300

    [+] playwright test project

---
+++ autotests/playwright/Saucedemo/Saucedemo/UnitTest1.cs
@@ -0,0 +1,49 @@
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+
+namespace Saucedemo
+{
+    [Parallelizable(ParallelScope.Self)]
+    public class Tests: PageTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public async Task ShouldLogin()
+        {
+            await using var browser = await Playwright.Firefox.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = false,
+                SlowMo = 50,
+            }) ;
+            var context = await browser.NewContextAsync();
+            // Open new page
+            var page = await context.NewPageAsync();
+            // Go to https://www.saucedemo.com/
+            await page.GotoAsync("https://www.saucedemo.com/");
+            // Click [data-test="username"]
+            await page.Locator("[data-test=\"username\"]").ClickAsync();
+            // Fill [data-test="username"]
+            await page.Locator("[data-test=\"username\"]").FillAsync("standard_user");
+            // Click [data-test="password"]
+            await page.Locator("[data-test=\"password\"]").ClickAsync();
+            // Fill [data-test="password"]
+            await page.Locator("[data-test=\"password\"]").FillAsync("secret_sauce");
+            // Click [data-test="login-button"]
+            await page.Locator("[data-test=\"login-button\"]").ClickAsync();
+             Assert.AreEqual("https://www.saucedemo.com/inventory.html", page.Url);
+        }
+
+        [Test]
+        public async Task ShouldMultiply()
+        {
+            int result = await Page.EvaluateAsync<int>("() => 7 * 3");
+            Assert.AreEqual(21, result);
+        }
+    }
+}
