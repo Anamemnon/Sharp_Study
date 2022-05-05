f115e571fc5b31af6dbec88769256774da76e4b6
Dmitry Chekir <chekirdmitry@gmail.com>
2022-05-05 22:40:17 +0300

    [+] ShouldNotLogin

--- autotests/playwright/Saucedemo/Saucedemo/TestLogin.cs
+++ autotests/playwright/Saucedemo/Saucedemo/TestLogin.cs
@@ -40,10 +40,31 @@ namespace Saucedemo
         }
 
         [Test]
-        public async Task ShouldMultiply()
+        public async Task ShouldNotLogin()
         {
-            int result = await Page.EvaluateAsync<int>("() => 7 * 3");
-            Assert.AreEqual(21, result);
+            await using var browser = await Playwright.Firefox.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = false,
+                SlowMo = 50,
+            });
+            var context = await browser.NewContextAsync();
+            // Open new page
+            var page = await context.NewPageAsync();
+            // Go to https://www.saucedemo.com/
+            await page.GotoAsync("https://www.saucedemo.com/");
+            // Click [data-test="username"]
+            await page.Locator("[data-test=\"username\"]").ClickAsync();
+            // Fill [data-test="username"]
+            await page.Locator("[data-test=\"username\"]").FillAsync("locked_out_user");
+            // Click [data-test="password"]
+            await page.Locator("[data-test=\"password\"]").ClickAsync();
+            // Fill [data-test="password"]
+            await page.Locator("[data-test=\"password\"]").FillAsync("secret_sauce");
+            // Click [data-test="login-button"]
+            await page.Locator("[data-test=\"login-button\"]").ClickAsync();
+            var expectedText = await page.Locator("[data-test=\"error\"]").TextContentAsync();
+            Assert.AreEqual("Epic sadface: Sorry, this user has been locked out.", expectedText);
+            //Assert.AreEqual("https://www.saucedemo.com/inventory.html", page.Url);
         }
     }
 }
